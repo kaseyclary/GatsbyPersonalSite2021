10b64c49c297b12b56894ca2e6acf4dd34a51be9
Kasey <mail.kaseyc@gmail.com>
2021-02-16 20:27:59 -0600

    fixed a couple small build issues. Removed Typewriter

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,34 +1,12 @@
 import React from 'react'
 import classes from './Header.module.css'
-import TypeWriterEffect from 'react-typewriter-effect'
 
 const header = () => (
     <div className={classes.HeaderContainer}>
         <div className={classes.HeaderImage}>
             <div className={classes.HeaderContentArea}>
                 <div className={classes.HeaderContent}>
-                <TypeWriterEffect
-                    textStyle={{
-                        fontFamily: 'Amatic SC',
-                        color: '#3F3D56',
-                        fontWeight: 700,
-                        fontSize: '4rem',
-                        textAlign: 'center',
-                    }}
-                    startDelay={500}
-                    cursorColor="#3F3D56"
-                    multiText={[
-                        'Thanks for Dropping in!',
-                        "I'm Kasey, the...",
-                        'Engineer',
-                        'Designer',
-                        'Thinker',
-                        'Builder',
-                        'Scroll on to learn more!'
-                    ]}
-                    multiTextDelay={1200}
-                    typeSpeed={30}
-                />
+                    <h2>Thanks for dropping in!</h2>
                 </div>
             </div>
         </div>
